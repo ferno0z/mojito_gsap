d15ed2fdac22c029e556436d1e7c3a7bbd2f5f12
ferno0z <deeznutsshinethru@gmail.com>
2026-05-12 16:07:56 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-5xl font-bold flex-center'>Welcome to my React App!</h1>
+    </>
+  )
+}
+
+export default App
